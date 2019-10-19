ae65cb0e10caa68c359a9751c3daa2e7710483fb
Yegor Bugayenko <yegor256@gmail.com>
2019-10-19 09:47:13 +0800

    #27 deploy

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+cd $(dirname $0)
+cp /code/home/assets/sttc/settings.xml .
+git add settings.xml
+git commit -m 'settings for dokku'
+trap 'git reset HEAD~1 && rm settings.xml' EXIT
+git push dokku master -f
+
