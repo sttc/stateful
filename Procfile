09e219239c5e44d67c535780a1f99ad03209b18e
Yegor Bugayenko <yegor256@gmail.com>
2025-01-27 11:14:27 +0300

    codecov-action v5

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 web: java -Xmx512m -cp target/stateful-jar-with-dependencies.jar co.stateful.Launch ${PORT} target/heroku-webapp
-
--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -e
 
-cd $(dirname $0)
+cd "$(dirname "$0")"
 cp /code/home/assets/sttc/settings.xml .
 git add settings.xml
 git commit -m 'settings for dokku'
 trap 'git reset HEAD~1 && rm settings.xml' EXIT
 git push heroku master -f
-
