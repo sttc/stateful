83c74a5cbbf861f9fd06a6750962551bcdf5242b
Yegor Bugayenko <yegor256@gmail.com>
2026-03-20 15:47:37 +0300

    Fix deploy script for Heroku deployment.

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,6 @@ set -e
 cd "$(dirname "$0")"
 cp /code/home/assets/sttc/settings.xml .
 git add settings.xml
-git commit -m 'settings for dokku'
+git commit -m 'settings for dokku' --no-verify
 trap 'git reset HEAD~1 && rm settings.xml' EXIT
 git push heroku master -f
